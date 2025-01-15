309721d23c432b34b68631e116cac3480f42b69b
Macrume <jakub.jarzabek49@gmail.com>
2025-01-15 01:38:18 +0100

    Zmiana random state.

--- Interface/interface_streamlit.py
+++ Interface/interface_streamlit.py
@@ -507,7 +507,7 @@ def main():
             st.stop()
 
         # Split the data into training and testing sets
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
         # Sidebar navigation with modern styling
         st.sidebar.title("Navigation 🧭")
